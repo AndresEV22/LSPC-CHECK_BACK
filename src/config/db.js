c407becd28779cf57643b9a892022632936a86a0
Andres Escobar Vélez <108491540+AndresEV22@users.noreply.github.com>
2023-03-27 12:17:19 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 module.exports ={
-    HOST:"containers-us-west-195.railway.app",//SERIA localhost en windows, esa es mi ip, entonces paila no hackearme
+    HOST:"containers-us-west-158.railway.app",//SERIA localhost en windows, esa es mi ip, entonces paila no hackearme
     USER:"postgres", //el usuario
-    PASSWORD:"wDQhIbVjPXQIPUbUGx3Y",//la password
+    PASSWORD:"eZWHu7zGMQVbmiXj1DuU",//la password
     DB:"railway",//el nombre de la base de datos que se usara
     dialect:"postgres", // el lenguaje sql a utilizar que es postgresql
-    port:7561
+    port:"6868"
 }
